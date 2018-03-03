921dfe41c5da10e84390ec36a4465ce682c271a5
Umer Amjad - H263353 <muhammadumer.amjad@honeywell.com>
2018-03-03 03:56:46 -0500

    More tests for absolute value parsing and substituting

--- Tests.hpp
+++ Tests.hpp
@@ -20,6 +20,9 @@ bool testAbsoluteValueCleaner(){
         assert(absoluteValueSubstitution("|x+(3x^2+|x+|x^|x-|x/|x+3|-1|+2|-3|+4|+17)+19x|").first == "<x+(3x^2+<x+<x^<x-<x/<x+3>-1>+2>-3>+4>+17)+19x>");
         assert(absoluteValueSubstitution("x+|x+5|+19").first == "x+<x+5>+19");
         assert(absoluteValueSubstitution("x+|x+(x^2+|x^3+(x-|5+3|*3)+5|-5)|+19").first == "x+<x+(x^2+<x^3+(x-<5+3>*3)+5>-5)>+19");
+        assert(absoluteValueSubstitution
+               ("x+|x+(x^2+|x^3+(x-|5+3|*3)-(x-|5+3|*3)*5|-5+(x-9|x+1|))+|x-1||+19").first ==
+                "x+<x+(x^2+<x^3+(x-<5+3>*3)-(x-<5+3>*3)*5>-5+(x-9<x+1>))+<x-1>>+19");
         assert(absoluteValueSubstitution("x+|x+(x^2+|x^3+(x-|5+3|*3)+5|-5)+|3x+5|+2|+19").first == "x+<x+(x^2+<x^3+(x-<5+3>*3)+5>-5)+<3x+5>+2>+19");
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
@@ -36,7 +39,6 @@ bool testAbsoluteValueEvaluator(){
         assert(parse("x+|x+(x^2+|x^3+(x-|5+3|*3)+5|-5)|+19")->eval(15) == 3640);
         assert(parse("x+|x+(x^2+|x^3+(x-|5+3|*3)+5|-5)|+19")->eval(-10.5) == 1290.375);
         assert(abs(parse("|x+|x^|x-|x/|x+3|-1|+2|-3|+4|")->eval(2) - 13.55606328618) < EPSILON);
-//        std::cout << std::setprecision(15) << parse("(|x+3|)(1+5)|x+(2+9|3-x|)*(x|x-2|)|")->eval(-2.357) << std::endl;
         assert(abs(parse("(|x+3|)(1+5)|x+(2+9|3-x|)*(x|x-2|)|")->eval(-2.357) - 1998.50897889355) < EPSILON);
         assert(parse("3+(5*(9+(x+3(x^2(|x +5( 13 + |x+5||x-5|(5 + 2|x^2+3|))+6( 13 + |x+5||x-5|(5 + 2|x^2+3|))|)))))")->eval(5)
                == 55573);
